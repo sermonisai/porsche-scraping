fd5cb66e765420cf832772756fa2ce29d97a9f3b
gcb-umkc <gcbz9k@mail.umkc.edu>
2022-06-16 10:24:36 +0200

    Fixed formatting on pdfToJSON

--- pdfToJSON.py
+++ pdfToJSON.py
@@ -2,11 +2,12 @@ from cgitb import text
 import fitz, sys, json, jsonlines
 
 def web_scraper(filename):
-
     doc = fitz.open(filename)
     text_blocks = []
+
     for page in doc:
         text = page.get_text("blocks")
+
         for block in text:
             if(block[4][0:7] == "<image:"):
                 continue
@@ -18,13 +19,13 @@ def web_scraper(filename):
                         "prompt" : "",
                         "paragraph" : block[4]
                     }
-                    text_blocks.append(dict)
-                    
-                    
-                    
-      filename = text_blocks[0]["paragraph"] + ".jsonl"
-      with open(filename, "w") as outfile:
-         for i in range(1,len(text_blocks)):
-           outfile.write(json.dumps(text_blocks[i]) + "\n")
-                
+
+                    text_blocks.append(dict)  
+
+    filename = text_blocks[0]["paragraph"] + ".jsonl"
+    
+    with open(filename, "w") as outfile:
+        for i in range(1,len(text_blocks)):
+            outfile.write(json.dumps(text_blocks[i]) + "\n")
+
 web_scraper(sys.arv[1])
