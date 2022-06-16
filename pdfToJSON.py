c871a81c48c7f8fcd5fe52a95ec1cdcaa3e19eaf
Lai-Justin <40769364+Lai-Justin@users.noreply.github.com>
2022-06-16 00:53:16 -0700

    Update pdfToJSON.py
    The base code to read in a pdf and to convert it into a JSONLines file.

--- pdfToJSON.py
+++ pdfToJSON.py
@@ -1,39 +1,30 @@
 from cgitb import text
-import fitz
-import sys
+import fitz, sys, json, jsonlines
 
 def web_scraper(filename):
+
     doc = fitz.open(filename)
     text_blocks = []
     for page in doc:
-    #    text_page = page.get_textpage()
-
-
-    #    text_blocks.append(text_page.extractText())
-
-    #for blocks in text_blocks:
-    #    print(blocks)
-
-
-    #for blocks in text_blocks:
-    #    print(blocks[4])
-    
-
         text = page.get_text("blocks")
         for block in text:
             if(block[4][0:7] == "<image:"):
                 continue
             else:
-                print(block[4])
+                if block[4].count(" ") > 5:
+                    dict = {
+                        "url" : "",
+                        "heading" : "",
+                        "prompt" : "",
+                        "paragraph" : block[4]
+                    }
+                    text_blocks.append(dict)
+                    
+                    
+                    
+      filename = text_blocks[0]["paragraph"] + ".jsonl"
+      with open(filename, "w") as outfile:
+         for i in range(1,len(text_blocks)):
+           outfile.write(json.dumps(text_blocks[i]) + "\n")
                 
-
-
-
-    #text_blocks.append(block.extractRAWDICT())
-
-
-
-    
-
-
-web_scraper(sys.argv[1])
+web_scraper(sys.arv[1])
